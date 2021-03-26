1f10c75cfeab2217fa76840bc2bb79f8b99b4bca
Matthew Paulin <matthewjpaulin@gmail.com>
2021-03-26 18:31:14 -0400

    broke everything

--- Application/boardzilla/src/App.js
+++ Application/boardzilla/src/App.js
@@ -4,46 +4,10 @@ import Widget from "./Components/Widget";
 function App() {
   return (
     <div className="App">
-      <Widget color={"green"}/>
-      <Widget color={"red"}/>
+      <Widget color={"green"} />
+      <Widget color={"red"} />
     </div>
   );
 }
 
 export default App;
-
-{/* <Draggable>
-<div>
-<ResizableBox width={500} height={500} style={{
-    // left: `${x}px`,
-    // top: `${y + props.y}px`,
-    backgroundColor: `${props.color}`
-}}>
-<div style={{
-    position: "relative",
-    width: `${width}px`,
-    height: `${height}px`,
-    zIndex: z,
-  }}
-  >
-  onMouseDown={handleMouseDown}Contents
-  
-</div>
-</ResizableBox>
-
-</div>
-</Draggable> 
-
-
-*/}
-
- {/* <Rnd style={{
-      backgroundColor: `${props.color}`
-    }}default={{
-    x: x,
-    y: y + props.y,
-    width: `${width}px`,
-    height: `${height}px`,
-  }} >
-dskalkkds
-    </Rnd> */}
--- Application/boardzilla/src/Components/Widget.js
+++ Application/boardzilla/src/Components/Widget.js
@@ -1,49 +1,21 @@
 import React, { useEffect, useState, useRef, useLayoutEffect } from "react";
-import { Resizable, ResizableBox } from 'react-resizable';
-
-
+import { Resizable, ResizableBox } from "react-resizable";
 
 export const Widget = (props) => {
-  //pass props telling us the window size
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-  const [z, setZ] = useState(0);
-  const [width, setWidth] = useState(500);
-  const [height, setHeight] = useState(500);
-  const [mouseDown, setMouseDown] = useState(false);
-  
-  useEffect(() => {
-    const handleMouseUp = () => setMouseDown(false);
-    window.addEventListener("mouseup", handleMouseUp);
-    return () => {
-      window.addEventListener("mouseup", handleMouseUp);
-    };
-  }, []);
-
-  useEffect(() => {
-    const handleMove = (event) => {
-      setX((x) => x + event.movementX);
-      setY((y) => y + event.movementY);
-    };
-    if (mouseDown) {
-      console.log("Clicked")
-      window.addEventListener("mousemove", handleMove);
-    }
-    return () => {
-      window.removeEventListener("mousemove", handleMove);
-    };
-  
-  }, [mouseDown]);
   return (
     //<div style={{ position: "relative", width: "auto", height: "auto"}}>
-    
-    <ResizableBox width={500} height={500} style={{
-      display: "inline-block",
-      left: `${x}px`,
-      top: `${y}px`,
-      backgroundColor: `${props.color}`
-    }}>
-    <div style={{
+
+    <ResizableBox
+      width={500}
+      height={500}
+      style={{
+        display: "inline-block",
+        left: `${0}px`,
+        top: `${0}px`,
+        backgroundColor: `${props.color}`,
+      }}
+    >
+      {/* <div style={{
       position: "relative",
       width: `${width}px`,
       height: `${height}px`,
@@ -51,9 +23,9 @@ export const Widget = (props) => {
     }}
     onMouseDown={() => setMouseDown(true)}>
     Contents
-  </div>
-  </ResizableBox>
-  //</div>
+  </div> */}
+    </ResizableBox>
+    //</div>
   );
 };
 
