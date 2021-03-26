91d791dcd98a7036754ddbb5fff9859d217b6f3a
Hargun <hargunbedi463@gmail.com>
2021-03-26 17:45:20 -0400

    Added resizability

--- Application/boardzilla/src/Components/Widget.js
+++ Application/boardzilla/src/Components/Widget.js
@@ -1,14 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, useLayoutEffect } from "react";
+import { Resizable, ResizableBox } from 'react-resizable';
+
+
 
 export const Widget = (props) => {
   //pass props telling us the window size
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
   const [z, setZ] = useState(0);
-  const [width, setWidth] = useState(50);
-  const [height, setHeight] = useState(50);
+  const [width, setWidth] = useState(500);
+  const [height, setHeight] = useState(500);
   const [mouseDown, setMouseDown] = useState(false);
-
+  
   useEffect(() => {
     const handleMouseUp = () => setMouseDown(false);
     window.addEventListener("mouseup", handleMouseUp);
@@ -23,28 +26,34 @@ export const Widget = (props) => {
       setY((y) => y + event.movementY);
     };
     if (mouseDown) {
+      console.log("Clicked")
       window.addEventListener("mousemove", handleMove);
     }
     return () => {
       window.removeEventListener("mousemove", handleMove);
     };
+  
   }, [mouseDown]);
-
   return (
-    <div style={{ position: "absolute", width: "100%", height: "100%" }}>
-      <div
-        style={{
-          position: "relative",
-          backgroundColor: `${props.color}`,
-          width: `${width}%`,
-          height: `${height}%`,
-          left: `${x}px`,
-          top: `${y}px`,
-          zIndex: z,
-        }}
-        onMouseDown={() => setMouseDown(true)}
-      ></div>
-    </div>
+    //<div style={{ position: "relative", width: "auto", height: "auto"}}>
+    
+    <ResizableBox width={500} height={500} style={{
+      display: "inline-block",
+      left: `${x}px`,
+      top: `${y}px`,
+      backgroundColor: `${props.color}`
+    }}>
+    <div style={{
+      position: "relative",
+      width: `${width}px`,
+      height: `${height}px`,
+      zIndex: z,
+    }}
+    onMouseDown={() => setMouseDown(true)}>
+    Contents
+  </div>
+  </ResizableBox>
+  //</div>
   );
 };
 
