d3903144977a97b09e766123957c8b24e6b3350b
Matthew Paulin <matthewjpaulin@gmail.com>
2021-03-27 02:12:18 -0400

    changed auth backend

---
+++ express-session-authentication-starter/server/database/schemas/index.js
@@ -0,0 +1,7 @@
+const Session = require("./Session");
+const User = require("./User");
+
+module.exports = {
+  Session,
+  User,
+};
---
+++ express-session-authentication-starter/server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+require("./config/environment");
+require("./database");
+
+const routes = require("./routes/index");
+const configPassport = require("./config/passport");
+
+const assetFolder = path.resolve(__dirname, "../dist/");
+const port = process.env.PORT;
+const app = express();
+
+app.use(express.static(assetFolder));
+app.use(express.json());
+
+configPassport(app, express);
+
+app.use("/", routes);
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
